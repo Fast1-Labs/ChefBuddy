95b4b413f887e9f070ea2e378e59e51ea1b8eb0b
anilyavas <anil.yavas99@gmail.com>
2024-11-18 19:52:26 +0300

    openai install and screen details

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -13,19 +13,14 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <HeaderButton />
-            </Link>
-          ),
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
         }}
       />
       <Tabs.Screen
         name="two"
         options={{
           title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="history" color={color} />,
         }}
       />
     </Tabs>
