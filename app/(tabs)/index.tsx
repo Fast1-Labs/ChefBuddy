a7909055ece4c180329151be5f68b5d718ff8ea2
anilyavas <anil.yavas99@gmail.com>
2024-11-18 19:58:52 +0300

    Home screen search bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,27 @@
 import { Stack } from 'expo-router';
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Text, View, TextInput, Pressable } from 'react-native';
 
 export default function Home() {
+  const [input, setInput] = useState('');
+
   return (
     <View className="flex-1">
       <Stack.Screen options={{ title: 'ChefBuddy' }} />
-      <Text className="p-2 text-xl font-bold">Hello there</Text>
+      <Text className="text-l p-2 text-center font-bold">
+        Please enter the ingredients you would like to use.
+      </Text>
+      <View className="m-2 flex-row items-center gap-2">
+        <TextInput
+          placeholder="Ingredients"
+          className="flex-1 border border-zinc-400 p-2"
+          value={input}
+          onChangeText={(item) => setInput(item)}
+        />
+        <Pressable>
+          <Text className=" font-bold text-blue-600">Search</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,22 +1,11 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+import { View, Text } from 'react-native';
 
 export default function Home() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Tab Two' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/two.tsx" title="Tab Two" />
-      </View>
-    </>
+    <View className="flex-1">
+      <Stack.Screen options={{ title: 'Recipe History' }} />
+      <Text>History</Text>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-});
