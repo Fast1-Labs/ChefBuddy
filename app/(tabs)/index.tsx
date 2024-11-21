e9e77881636a9c713930edb123b3505d56f21eb8
anilyavas <anil.yavas99@gmail.com>
2024-11-21 18:27:20 +0300

    home screen basic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,44 @@
-import { Stack } from 'expo-router';
-import { View } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+import { useState } from 'react';
+import { Button, View, Text } from 'react-native';
 
 import IngredientInput from '~/components/IngredientInput';
+import { fetchMealSuggestions } from '~/utils/api';
 
 export default function Home() {
-  const onAdd = () => {
-    console.log('Add');
+  const [ingredients, setIngredients] = useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleAddIngredient = (ingredient: string) => {
+    setIngredients([...ingredients, ingredient]);
+  };
+
+  const handleGenerateMeals = async () => {
+    setLoading(true);
+    try {
+      const suggestions = await fetchMealSuggestions(ingredients);
+      router.push({
+        pathname: '/two',
+        params: { suggestions },
+      });
+    } catch (error) {
+      console.log('Error while fetching meals.', error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <View className="flex-1">
+    <View className="flex-1 p-2">
       <Stack.Screen options={{ title: 'ChefBuddy' }} />
-      <IngredientInput onAdd={onAdd} />
+      <IngredientInput onAdd={handleAddIngredient} />
+      {ingredients.map((ingredient, index) => (
+        <Text key={index} className="mt-2 text-lg font-bold">
+          {ingredient}
+        </Text>
+      ))}
+      <Button title="Generate Meals" onPress={handleGenerateMeals} disabled={loading} />
     </View>
   );
 }
