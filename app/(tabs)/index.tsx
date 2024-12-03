3aa5b97df8d58a58c6f1285c113ec5f89b7808ad
anilyavas <anil.yavas99@gmail.com>
2024-12-03 13:35:53 +0300

    Displaying GPT offered recipe from ingredients

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,54 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { OpenAI } from 'openai';
 import { useState } from 'react';
-import { View, Dimensions, TextInput, Button } from 'react-native';
+import {
+  View,
+  Dimensions,
+  TextInput,
+  Button,
+  ActivityIndicator,
+  Alert,
+  ScrollView,
+  Text,
+} from 'react-native';
 
 export default function Home() {
   const openai = new OpenAI({
     apiKey: process.env.EXPO_PUBLIC_OPENAI_API_KEY,
   });
 
-  const [ingredients, setIngredients] = useState('');
+  const [ingredients, setIngredients] = useState<string>('');
+  const [recipe, setRecipe] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
 
   const generateRecipes = async (ingredients: string) => {
-    const response = await openai.chat.completions.create({
-      model: 'gpt-4',
-      messages: [
-        {
-          role: 'system',
-          content:
-            'You are a culinary assistant. Your goal is to provide the user with a recipe that best utilizes their provided ingredients. Make the recipe easy to follow, include clear ingredient quantities, step-by-step instructions, and provide preparation and cooking times. Tailor your suggestions to be creative, practical, and enjoyable.',
-        },
-        {
-          role: 'user',
-          content: `I have the following ingredients: ${ingredients}. Can you suggest a recipe I can prepare? Include a recipe name, picture, ingredients with quantities, and step by step cooking instructions. Please also mention the preparation and cooking time.`,
-        },
-      ],
-    });
+    try {
+      setLoading(true);
+      setRecipe(null);
+      const response = await openai.chat.completions.create({
+        model: 'gpt-4',
+        messages: [
+          {
+            role: 'system',
+            content:
+              'You are a culinary assistant. Your goal is to provide the user with a recipe that best utilizes their provided ingredients. Make the recipe easy to follow, include clear ingredient quantities, step-by-step instructions, and provide preparation and cooking times. Tailor your suggestions to be creative, practical, and enjoyable.',
+          },
+          {
+            role: 'user',
+            content: `I have the following ingredients: ${ingredients}. Can you suggest a recipe I can prepare? Include a recipe name, picture, ingredients with quantities, and step by step cooking instructions. Please also mention the preparation and cooking time.`,
+          },
+        ],
+        max_tokens: 700,
+        temperature: 0.7,
+      });
+      const recipe = response.choices?.[0].message.content?.trim();
+      setRecipe(recipe);
+    } catch (error) {
+      Alert.alert('Error while connecting to GPT.');
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -42,6 +66,12 @@ export default function Home() {
           />
           <Button title="Search" color="white" onPress={() => generateRecipes(ingredients)} />
         </View>
+        {loading && <ActivityIndicator size="large" color="#ffffff" className="mt-5" />}
+        {recipe && (
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Text className="p-4 text-lg text-white">{recipe}</Text>
+          </ScrollView>
+        )}
       </LinearGradient>
     </View>
   );
