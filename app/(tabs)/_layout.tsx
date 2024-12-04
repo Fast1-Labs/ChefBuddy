59c54f3a103ce892fa5a52181a751add4b3c5091
anilyavas <anil.yavas99@gmail.com>
2024-12-04 12:49:56 +0300

    type decleration for auth

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,13 @@
-import { Stack } from 'expo-router';
+import { Redirect, Stack } from 'expo-router';
+
+import { useAuth } from '~/context/AuthContext';
 
 export default function TabLayout() {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Redirect href="/(auth)/auth" />;
+  }
   return (
     <Stack screenOptions={{ headerStyle: { backgroundColor: 'orange' } }}>
       <Stack.Screen name="index" options={{ title: 'ChefBuddy' }} />
--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,10 +1,15 @@
-import { Session } from '@supabase/supabase-js';
+import { Session, User } from '@supabase/supabase-js';
 import { createContext, PropsWithChildren, useContext, useEffect, useState } from 'react';
 import { ActivityIndicator } from 'react-native';
 
 import { supabase } from '~/utils/supabase';
 
-const AuthContext = createContext({});
+type AuthContextType = {
+  session: Session | null;
+  user: User | null;
+};
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
 export default function AuthContextProvider({ children }: PropsWithChildren) {
   const [session, setSession] = useState<Session | null>(null);
   const [isReady, setIsReady] = useState(false);
@@ -19,14 +24,16 @@ export default function AuthContextProvider({ children }: PropsWithChildren) {
       setSession(session);
     });
 
-    return () => subscription?.unsubscribe();
+    return () => subscription?.subscription.unsubscribe();
   }, []);
 
   if (!isReady) {
     return <ActivityIndicator />;
   }
   return (
-    <AuthContext.Provider value={{ session, user: session?.user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ session, user: session?.user || null }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
 
