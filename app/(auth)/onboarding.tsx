cec18950bb6c5728b5d0a3b7edeb82f52e0ad4ae
anilyavas <anil.yavas99@gmail.com>
2024-12-06 18:36:59 +0300

    basic UI of onboarding slide animation and content

--- app/(auth)/onboarding.tsx
+++ app/(auth)/onboarding.tsx
@@ -1,6 +1,7 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { LinearGradient } from 'expo-linear-gradient';
 import { router } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 import { useRef, useState } from 'react';
 import {
   View,
@@ -30,21 +31,46 @@ export default function OnBoarding() {
     router.push('./login');
   };
   return (
-    <LinearGradient
-      colors={['#833ab4', '#fd1d1d', '#fcb045']}
-      style={{ height: Dimensions.get('window').height }}>
-      <Pressable onPress={handleSkip} className="absolute right-5 top-20 z-10">
-        <Text className="font-bold text-white">SKIP</Text>
-      </Pressable>
-      <ScrollView onScroll={handleScroll}>
-        {onBoardingData.map((item: onBoardingDataType, index: number) => (
-          <View key={index} className="flex-1 items-center justify-center gap-2 p-4">
-            {item.image}
-            <Text className="text-center text-2xl font-bold text-white">{item.title}</Text>
-            <Text className="text-center text-lg font-semibold text-gray-100">{item.subtitle}</Text>
-          </View>
-        ))}
-      </ScrollView>
-    </LinearGradient>
+    <View className="flex-1">
+      <LinearGradient
+        colors={['#833ab4', '#fd1d1d', '#fcb045']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={{
+          height: Dimensions.get('window').height,
+          top: 0,
+          left: 0,
+          right: 0,
+          alignItems: 'center',
+          justifyContent: 'center',
+          flex: 1,
+        }}>
+        <StatusBar style="light" />
+        <Pressable onPress={handleSkip} className="absolute right-10 top-20 z-10">
+          <Text className="font-bold text-white">SKIP</Text>
+        </Pressable>
+        <ScrollView
+          onScroll={handleScroll}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          ref={scrollViewRef}>
+          {onBoardingData.map((item: onBoardingDataType, index: number) => (
+            <View
+              key={index}
+              className="items-center justify-center"
+              style={{ width: Dimensions.get('window').width }}>
+              {item.image}
+              <View className="gap-6 p-4">
+                <Text className=" text-center text-3xl font-bold text-white">{item.title}</Text>
+                <Text className=" text-center text-lg font-semibold text-gray-100">
+                  {item.subtitle}
+                </Text>
+              </View>
+            </View>
+          ))}
+        </ScrollView>
+      </LinearGradient>
+    </View>
   );
 }
