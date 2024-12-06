3c943474e597275aec8db9fc62be1f0ac3b0875e
anilyavas <anil.yavas99@gmail.com>
2024-12-06 18:22:09 +0300

    skip and scroll functions

--- app/(auth)/onboarding.tsx
+++ app/(auth)/onboarding.tsx
@@ -1,17 +1,50 @@
-import { View, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { LinearGradient } from 'expo-linear-gradient';
+import { router } from 'expo-router';
+import { useRef, useState } from 'react';
+import {
+  View,
+  Text,
+  Dimensions,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+  ScrollView,
+  Pressable,
+} from 'react-native';
 
 import { onBoardingData, onBoardingDataType } from '~/constants/onboard';
 
 export default function OnBoarding() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const scrollViewRef = useRef<ScrollView>(null);
+
+  const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const contentOffSetX = event.nativeEvent.contentOffset.x;
+    const currentIndex = Math.round(contentOffSetX / event.nativeEvent.layoutMeasurement.width);
+    if (currentIndex !== activeIndex) {
+      setActiveIndex(currentIndex);
+    }
+  };
+  const handleSkip = async () => {
+    await AsyncStorage.setItem('onboarding', 'true');
+    router.push('./login');
+  };
   return (
-    <View className="flex-1 items-center justify-center">
-      {onBoardingData.map((item: onBoardingDataType, index: number) => (
-        <View key={index} className="flex-1 items-center">
-          {item.image}
-          <Text className="text-center text-2xl font-bold">{item.title}</Text>
-          <Text className="text-center text-lg font-semibold">{item.subtitle}</Text>
-        </View>
-      ))}
-    </View>
+    <LinearGradient
+      colors={['#833ab4', '#fd1d1d', '#fcb045']}
+      style={{ height: Dimensions.get('window').height }}>
+      <Pressable onPress={handleSkip} className="absolute right-5 top-20 z-10">
+        <Text className="font-bold text-white">SKIP</Text>
+      </Pressable>
+      <ScrollView onScroll={handleScroll}>
+        {onBoardingData.map((item: onBoardingDataType, index: number) => (
+          <View key={index} className="flex-1 items-center justify-center gap-2 p-4">
+            {item.image}
+            <Text className="text-center text-2xl font-bold text-white">{item.title}</Text>
+            <Text className="text-center text-lg font-semibold text-gray-100">{item.subtitle}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </LinearGradient>
   );
 }
