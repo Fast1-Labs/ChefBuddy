d50f61c7aef3f6d3952b069a63a293e38de2827d
anilyavas <anil.yavas99@gmail.com>
2024-12-06 18:14:21 +0300

    onboarding display basic

--- app/(auth)/onboarding.tsx
+++ app/(auth)/onboarding.tsx
@@ -1,18 +1,17 @@
-import { View, FlatList, Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 import { onBoardingData, onBoardingDataType } from '~/constants/onboard';
 
 export default function OnBoarding() {
   return (
     <View className="flex-1 items-center justify-center">
-      <FlatList
-        data={onBoardingData}
-        renderItem={({ item }: { item: onBoardingDataType }) => (
-          <View key={item.id}>
-            <Text>{item.title}</Text>
-          </View>
-        )}
-      />
+      {onBoardingData.map((item: onBoardingDataType, index: number) => (
+        <View key={index} className="flex-1 items-center">
+          {item.image}
+          <Text className="text-center text-2xl font-bold">{item.title}</Text>
+          <Text className="text-center text-lg font-semibold">{item.subtitle}</Text>
+        </View>
+      ))}
     </View>
   );
 }
--- constants/onboard.tsx
+++ constants/onboard.tsx
@@ -1,3 +1,5 @@
+import { Image } from 'react-native';
+
 import Onboarding3 from '../assets/onboarding/ai.png';
 import Onboarding1 from '../assets/onboarding/diet.png';
 import Onboarding2 from '../assets/onboarding/salad.png';
@@ -14,18 +16,18 @@ export const onBoardingData: onBoardingDataType[] = [
     id: 1,
     title: 'Start with Your Ingredients',
     subtitle: 'Simply enter the ingredients you have, and let ChefBuddy do the magic.',
-    image: <Onboarding1 />,
+    image: <Image source={Onboarding1} style={{ width: 200, height: 200 }} />,
   },
   {
     id: 2,
     title: 'Get Tailored Recipes',
     subtitle: 'ChefBuddy finds the best recipes based on what you already have in your kitchen.',
-    image: <Onboarding2 />,
+    image: <Image source={Onboarding2} style={{ width: 200, height: 200 }} />,
   },
   {
     id: 3,
     title: 'Cook & Enjoy!',
     subtitle: 'Follow easy step-by-step instructions and enjoy meals made just for you.',
-    image: <Onboarding3 />,
+    image: <Image source={Onboarding3} style={{ width: 200, height: 200 }} />,
   },
 ];
