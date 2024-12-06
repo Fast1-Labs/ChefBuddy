3368a4a5e863b973c17c507d4d3e6988748cd937
anilyavas <anil.yavas99@gmail.com>
2024-12-06 17:35:42 +0300

    onboarding assets and screen

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from 'expo-router';
 
 export default function LoginLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="onboarding" />
+      <Stack.Screen name="login" />
+    </Stack>
+  );
 }
---
+++ app/(auth)/onboarding.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function OnBoarding() {
+  return (
+    <View>
+      <Text>Welcome onboard</Text>
+    </View>
+  );
+}
